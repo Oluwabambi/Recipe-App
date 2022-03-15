4b63794894560a26ee0cec6563136fe41a8697ec
Bambi <bawe97@gmail.com>
2022-03-15 14:05:14 +0100

    added crud operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   title = 'WebMagnat-frontend-test';
   dialogBox=false;
   allRecipes: any;
+  currentRecipe: any = '';
+  currentIngredients = []
+  currentDirections = []
 
 
   constructor (private fb: FormBuilder, private dataService: DataService) {}
@@ -29,18 +32,39 @@ export class AppComponent {
     this.dataService.getRecipes().subscribe( res => {
       this.allRecipes = res;
       console.log(res);
-      
+      this.currentRecipe = this.allRecipes[0];
+      this.currentIngredients = this.currentRecipe.ingredients.split("*");
+      this.currentDirections = this.currentRecipe.directions.split("*"); 
     })
+  }
+
+  selectedRecipe(recipe:any) {
+    this.currentRecipe = recipe;
+    this.currentIngredients = this.currentRecipe.ingredients.split("*");
+    this.currentDirections = this.currentRecipe.directions.split("*");
   }
 
   addNewRecipe() {
     this.dialogBox = true;
     let bod: any = document.querySelector('body');
     console.log(bod);
+    
   }
 
   submitRecipe() {
+    this.dataService.addRecipe(this.recipeForm.value).subscribe( res => {
+      console.log(res);
+      this.dialogBox = false;
+      this.showRecipes();
+    })
+  }
 
+  removeRecipe(id:any) {
+    if(confirm("Are you sure you want to delete?")){
+      this.dataService.deleteRecipe(id).subscribe(()=> {
+        this.showRecipes();
+      })
+    }
   }
 
   removeDialog() {
