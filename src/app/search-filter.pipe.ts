8928ac8d11c6b19400f0f7a3b25520859ed20656
Bambi <bawe97@gmail.com>
2022-03-21 18:45:39 +0100

    added pipe

--- src/app/search-filter.pipe.ts
+++ src/app/search-filter.pipe.ts
@@ -11,29 +11,35 @@ export class SearchFilterPipe implements PipeTransform {
 
       
       // let numArgs: any = args.split(" ")
-      const implodeValue = args.includes(" ") ? args.split(" "): null;
+      // const implodeValue = args.includes(" ") ? args.split(" "): null;
       // if (numArgs.length>1) {
       //   return numArgs.every((a: any) => value.includes(a))
       // }
 
       
 
-       const filteredResult = value.filter(function(data:any){
-        const ingedientArray = data.ingredients.split("*");
-        for (let i of ingedientArray) { 
-          let arrind = ingedientArray.indexOf(i)
-          ingedientArray[arrind] = ingedientArray[arrind].trim()
-      }
-        const result = implodeValue.every((i:any) => ingedientArray.includes(i))
-        console.log(ingedientArray, implodeValue, 'test result');
-        return result;
+      //  const filteredResult = value.filter(function(data:any){
+      //   const ingedientArray = data.ingredients.split("*");
+      //   for (let i of ingedientArray) { 
+      //     let arrind = ingedientArray.indexOf(i)
+      //     ingedientArray[arrind] = ingedientArray[arrind].trim()
+      // }
+      //   const result = implodeValue.every((i:any) => ingedientArray.includes(i))
+      //   console.log(ingedientArray, implodeValue, 'test result');
+      //   return result;
         
-        //  const test = data.ingredients.toLowerCase().includes(args);
-        //   return data.ingredients.toLowerCase().includes(args);
-      });
-      console.log(filteredResult, 'test resiult');
+      //   //  const test = data.ingredients.toLowerCase().includes(args);
+      //   //   return data.ingredients.toLowerCase().includes(args);
+      // });
+      // console.log(filteredResult, 'test resiult');
       
-      return filteredResult;
+      // return filteredResult;
+
+      args = args.toLowerCase();
+
+      return value.filter(function(data:any){
+          return JSON.stringify(data).toLowerCase().includes(args);
+      });
 
   }
 
